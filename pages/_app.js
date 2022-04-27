4dbe5379352fca9187035163784e455649f60cb6
yasen002 <46136113+yasen002@users.noreply.github.com>
2022-04-26 23:54:58 -0700

    [add] env. [debug] redirect mismatch

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
-import Amplify from "aws-amplify";
+import { Amplify } from "aws-amplify";
 import "../styles/globals.css";
 import awsExports from "../src/aws-exports.js";
+
+awsExports.oauth.redirectSignIn = `${process.env.NEXT_PUBLIC_REDIRECT_URL}`;
+awsExports.oauth.redirectSignOut = `${process.env.NEXT_PUBLIC_REDIRECT_URL}`;
 
 Amplify.configure({ ...awsExports, ssr: true });
 function MyApp({ Component, pageProps }) {
