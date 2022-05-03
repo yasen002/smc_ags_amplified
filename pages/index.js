eb60ed4bcb51e6f8f957627a8f1fe0ca033b97a4
yasen002 <46136113+yasen002@users.noreply.github.com>
2022-05-02 23:45:42 -0700

    [add] async await [bug] data loading fix. [clean] async logic

--- pages/index.js
+++ pages/index.js
@@ -6,74 +6,76 @@ import { Auth, Hub } from "aws-amplify";
 import Layout from "../Component/layouts/Layout";
 import Dashboard from "../Component/Dashboard";
 import NotFound from "../Component/NotFound";
+import Loading from "../Component/Loading";
+
+function resolveAfter3Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 3000);
+  });
+}
+
+function getUser() {
+  return Auth.currentAuthenticatedUser();
+}
+
+async function getStudent(email) {
+  await resolveAfter3Seconds();
+  return await DataStore.query(Student, (c) => c.email("eq", email));
+}
 
 export default function Home() {
   const [user, setUser] = useState(null);
   const [studentData, setStudentData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const mountedRef = useRef(true);
-  useEffect(() => {
-    if (!mountedRef.current) return null;
 
+  useEffect(() => {
+    // install Amplify user hub
+    if (!mountedRef.current) return null;
     Hub.listen("auth", ({ payload: { event, data } }) => {
       switch (event) {
         case "signIn":
         case "cognitoHostedUI":
-          setLoading(true);
-          getUser().then((userData) => setUser(userData));
+          getUser()
+            .then((userData) => {
+              setUser(userData);
+              return getStudent(userData.attributes.email);
+            })
+            .then((studentData) => {
+              if (studentData.length > 0) {
+                setStudentData(studentData[0]);
+              } else {
+                setStudentData(null);
+              }
+              setLoading(false);
+            })
+            .catch((e) => console.log("index.js-> Error in getting user info: \n", e));
           break;
         case "signOut":
           setUser(null);
           setStudentData(null);
-          setLoading(false);
+          setLoading(true);
           break;
         case "signIn_failure":
         case "cognitoHostedUI_failure":
-          console.log("Sign in failure", data);
           break;
       }
     });
-    setLoading(true);
-    getUser().then((userData) => setUser(userData));
+
     return () => {
       mountedRef.current = false;
     };
-  }, []);
+  }, [loading]);
 
-  useEffect(() => {
-    if (!mountedRef.current) return null;
-    if (user) {
-      getStudent(user.attributes.email).then((student) => {
-        if (student.length > 0) {
-          setStudentData(student);
-        } else {
-          setStudentData(null);
-        }
-        setLoading(false);
-      });
-    }
-  }, [user, studentData]);
-
-  function getUser() {
-    return Auth.currentAuthenticatedUser()
-      .then((userData) => userData)
-      .catch(() => console.log("Not signed in"));
-  }
-
-  async function getStudent(email) {
-    const student = await DataStore.query(Student, (c) => c.email("eq", email));
-    return student;
-  }
   return (
     <>
       <Layout info={user ? { email: user.attributes.email } : false}>
-        <div className={styles.center}>
-          <div className={styles.container}>
-            {loading && user && <h1>Loading ... </h1>}
-            {!loading && studentData && <Dashboard data={studentData[0]} />}
-            {!loading && !studentData && <NotFound />}
-          </div>
-        </div>
+        {loading && user && <Loading />}
+        {!user && !loading && <h1>Please Sign in </h1>}
+        {!loading && studentData && <Dashboard data={studentData} />}
+        {!loading && !studentData && <NotFound />}
       </Layout>
     </>
   );
