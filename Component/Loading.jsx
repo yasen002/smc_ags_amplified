59d8837dbb33a172f9f8ddae02b61b48fafc2668
yasen002 <46136113+yasen002@users.noreply.github.com>
2022-05-02 23:46:01 -0700

    [add] Loading component

---
+++ Component/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import $ from "./Loading.module.scss";
+export default function Loading({ width = "65px", height = "65px" }) {
+  return (
+    <svg className={$.spinner} width={width} height={height} viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+      <circle className={$.path} fill="none" strokeWidth={6} strokeLinecap="round" cx={33} cy={33} r={30} />
+    </svg>
+  );
+}
